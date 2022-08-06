5ea15a9793fa7af8e97021d2f5fc50b159403419
Mieha Ozdemir <mieha.ozdemir@icloud.com>
2022-08-05 19:53:48 -0800

    trying to create login
    attempting to create login, commented out all starter code

--- github-portfolio-project-front-end-banking-app/login.js
+++ github-portfolio-project-front-end-banking-app/login.js
@@ -1,13 +1,15 @@
 function Login(){
-  const ctx = React.useContext(UserContext);
 
-  return (
-    <Card
-    txtcolor="black"
-    header="BadBank Login to User Account"
-    title="Login"
-    text={JSON.stringify(ctx)}
-    body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)} 
-    /> 
-  ); 
 }
+ //const ctx = React.useContext(UserContext);
+
+  //return (
+    //<Card
+    //txtcolor="black"
+    //header="BadBank Login to User Account"
+    //title="Login"
+    //text={JSON.stringify(ctx)}
+    //body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)} 
+    ///> 
+  //); 
+//}
